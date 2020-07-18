096aab5cbdf1e17b56d850fe04234c7d372c6822
raqwel <yuto.imamu@gmail.com>
2020-07-18 16:10:04 +0900

    [WIP] Modify Multi Thread Server

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,12 @@
+extern crate HttpServer;
+
+use HttpServer::ThreadPool;
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::fs::File;
+use std::thread;
+use std::time::Duration;
+use std::thread::Thread;
 
 static STATUS_LINE_200: &str = "HTTP/1.1 200 OK";
 static STATUS_LINE_404: &str = "HTTP/1.1 404 Not found";
@@ -8,12 +14,14 @@ static STATUS_LINE_404: &str = "HTTP/1.1 404 Not found";
 fn main() {
     let addr = "127.0.0.1:7878";
     let listener = TcpListener::bind(addr).unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection Established!");
 
-        handle_connection(stream);
+        pool.execute(||
+            handle_connection(stream));
 
     }
 }
@@ -23,10 +31,16 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get_request = b"GET / HTTP/1.1\r\n";
+    let sleep_request = b"GET /sleep HTTP/1.1\r\n";
+
 
     let (file_path, status_line) = if buffer.starts_with(get_request) {
         ("index.html", STATUS_LINE_200)
-    } else {
+    } else if buffer.starts_with(sleep_request) {
+        thread::sleep(Duration::from_secs(5));
+        ("index.html", STATUS_LINE_200)
+    }
+    else {
         ("404.html", STATUS_LINE_404)
     };
 
